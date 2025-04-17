e08bf6edc97cc194b846d2149de315ed60a3550d
nga <nthinga2003@gmail.com>
2025-04-17 20:21:26 +0700

    sua lai nhan yeu cau xuat san pham

--- class/clsdsnhanyeucauxuatSP.php
+++ class/clsdsnhanyeucauxuatSP.php
@@ -1,28 +1,4 @@
 <?php
-// include('ketnoi.php'); // Đảm bảo bạn đã kết nối với cơ sở dữ liệu
-// // Lấy giá trị của tham số 'id' từ URL
-// $id = isset($_GET['id']) ? $_GET['id'] : ''; // Nếu không có 'id', gán giá trị rỗng
-
-// // Kiểm tra nếu không có id, chuyển hướng hoặc thông báo lỗi
-// if (empty($id)) {
-//     echo "Không có id người dùng!";
-//     exit(); // Dừng thực thi chương trình
-// }
-// $kho = new quanlikho(); // Lớp kết nối của bạn
-// $conn = $kho->connect(); // Lấy kết nối từ phương thức connect()
-// // Truy vấn để lấy danh sách nhân viên kho từ cơ sở dữ liệu
-
-// $danhsach = array();
-// $sql = "SELECT yc.idYeuCauXuatSP, yc.ngayYeuCau, yc.trangThai,kh.hoTen
-//         FROM yeucauxuatsanpham yc
-//         JOIN khachhang kh ON yc.idKhachHang = kh.idKhachHang";
-// $result = $conn->query($sql);
-// if ($result && $result->num_rows > 0) {
-//     while ($row = $result->fetch_assoc()) {
-//         $danhsach[] = $row;
-//     }
-// }
-
 class qlykho{
     public function connect()
 	{
@@ -137,9 +113,9 @@ class qlykho{
 			$dem=1;
 			while($row=mysql_fetch_array($ketqua))
 			{
-                $idSP = $row['idSanPham'];
-                $laytenSP= $this->laycot("select tensanPham from sanpham sp join chitietyeucauxuatsp ct on sp.idSanPham = ct.idSanPham where ct.idSanPham = '$idSP' ");
-                $laysoluongSP= $this->laycot("select soLuongXuat from chitietyeucauxuatsp where idSanPham='$idSP'");
+                $maSP = $row['maSP'];
+                $laytenSP= $this->laycot("select tensanPham from sanpham sp join chitietyeucauxuatsanpham ct on sp.maSP = ct.maSP where ct.maSP = '$maSP' ");
+                $laysoluongSP= $this->laycot("select soLuongXuat from chitietyeucauxuatsanpham where maSP='$maSP'");
                 echo ''.$laytenSP.' x số lượng: '.$laysoluongSP.' <br>';
 				
 			}   
